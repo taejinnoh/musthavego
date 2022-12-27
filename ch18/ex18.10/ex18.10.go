f9391a7bff10cd43b853953c0cde87cf6eb93c1a
taejin_noh <taejin_noh@innodep.com>
2022-12-27 17:08:26 +0900

    feat: 슬라이스 요소 삭제

---
+++ ch18/ex18.10/ex18.10.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice := []int{1, 2, 3, 4, 5, 6}
+	idx := 2
+
+	//for i := idx + 1; i < len(slice); i++ {
+	//	slice[i-1] = slice[i]
+	//}
+	//slice = slice[:len(slice)-1]
+	//fmt.Println(slice)
+
+	slice = append(slice[:idx], slice[idx+1:]...)
+	fmt.Println(slice)
+}
