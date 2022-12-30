183aac31435d3168f0c18430e2dbad7a665e7a73
taejin_noh <taejin_noh@innodep.com>
2022-12-30 13:25:18 +0900

    feat: 단어 검색 프로그램 기능 추가 - 현재 폴더의 하위 폴더까지 검색하는 기능 추가

---
+++ ch26/ex26.5/ex26.5.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// 찾은 라인 정보
+type LineInfo struct {
+	lineNo int
+	line   string
+}
+
+// 파일 내 라인 정보
+type FileInfo struct {
+	filename string
+	lines    []LineInfo
+}
+
+const rootPath = "."
+
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("2개 이상의 실행 인수가 필요합니다. ex) ex26.1 word filepath")
+		return
+	}
+	word := os.Args[1]
+	//word := "brother"
+	fmt.Println("찾으려는 단어:", word)
+
+	files := os.Args[2:]
+	//files := []string{"*.txt"}
+	fileInfos := []FileInfo{}
+	for _, path := range files {
+		fileInfos = append(fileInfos, FindWordInAllFiles(word, path)...)
+	}
+
+	for _, fileInfo := range fileInfos {
+		fmt.Println(fileInfo.filename)
+		fmt.Println("------------------------------")
+		for _, lineInfo := range fileInfo.lines {
+			fmt.Println("\t", lineInfo.lineNo, "\t", lineInfo.line)
+		}
+		fmt.Println("------------------------------\n")
+	}
+}
+
+func FindWordInAllFiles(word, path string) []FileInfo {
+	fileInfos := []FileInfo{}
+
+	filelist, err := GetFileList(path)
+	if err != nil {
+		fmt.Println("파일 경로가 잘못되었습니다. err:", err, "path:", path)
+		return fileInfos
+	}
+
+	ch := make(chan FileInfo)
+	cnt := len(filelist)
+	receiveCnt := 0
+
+	for _, filename := range filelist {
+		fmt.Println("goroutine start")
+		go FindWordInFile(word, filename, ch)
+	}
+
+	if len(filelist) == 0 {
+		fmt.Println("파일이 없습니다.")
+		close(ch)
+	}
+
+	for fileInfo := range ch {
+		fmt.Println("channel output")
+		fileInfos = append(fileInfos, fileInfo)
+		receiveCnt++
+		if receiveCnt == cnt {
+			break
+		}
+	}
+	return fileInfos
+}
+
+// 하위 폴더의 파일까지 검색
+func GetFileList(pattern string) ([]string, error) {
+	filelist := []string{}
+	err := filepath.Walk(rootPath, func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() {
+			matched, _ := filepath.Match(pattern, info.Name())
+			if matched {
+				filelist = append(filelist, path)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		return []string{}, nil
+	}
+
+	return filelist, nil
+}
+
+func FindWordInFile(word, filename string, ch chan FileInfo) {
+	fileInfo := FileInfo{filename, []LineInfo{}}
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("파일을 찾을 수 없습니다. ", filename)
+		ch <- fileInfo
+		return
+	}
+	defer file.Close()
+
+	lineNo := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineNo++
+		line := scanner.Text()
+		if strings.Contains(line, word) {
+			fileInfo.lines = append(fileInfo.lines, LineInfo{lineNo, line})
+		}
+	}
+	ch <- fileInfo
+	fmt.Println("channel input")
+}
---
+++ ch26/ex26.5/go.mod
@@ -0,0 +1,3 @@
+module ch26/ex26.5
+
+go 1.19
