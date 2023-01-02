8b9a716c95be1dc2521c2c12b8a4c84c89ca0cbe
taejin_noh <taejin_noh@innodep.com>
2023-01-02 15:47:18 +0900

    feat: json 데이터 전송 - 테스트 코드 추가

---
+++ ch29/ex29.6/ex29_6_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/student", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	student := new(Student)
+	err := json.NewDecoder(res.Body).Decode(student)
+	assert.Nil(err)
+	assert.Equal("aaa", student.Name)
+	assert.Equal(16, student.Age)
+	assert.Equal(87, student.Score)
+}
