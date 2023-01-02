6d2cf9f8952053c3e30e961ab79637e9a7e869e9
taejin_noh <taejin_noh@innodep.com>
2023-01-02 10:57:15 +0900

    feat:외부 테스트 패키지로 변경 - stretchr/testify 패키지로 테스트 코드 작성

--- ch28/ex28.1/ex28_1_test.go
+++ ch28/ex28.1/ex28_1_test.go
@@ -1,17 +1,16 @@
 package main
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestSquare1(t *testing.T) {
-	rst := square(9)
-	if rst != 81 {
-		t.Errorf("square(9) should be 81 but square(9) returns %d", rst)
-	}
+	assert := assert.New(t)
+	assert.Equal(81, square(9), "square(9) should be 81")
 }
 
 func TestSquare2(t *testing.T) {
-	rst := square(3)
-	if rst != 9 {
-		t.Errorf("square(3) should be 81 but square(3) returns %d", rst)
-	}
+	assert := assert.New(t)
+	assert.Equal(9, square(3), "square(3) should be 9")
 }
