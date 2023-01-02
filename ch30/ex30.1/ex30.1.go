e7961e0453d1ae306e88f21f16f76d9f1dedba0b
taejin_noh <taejin_noh@innodep.com>
2023-01-02 17:17:01 +0900

    feat: 학생 리스트 GET API 작성

---
+++ ch30/ex30.1/ex30.1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+	"sort"
+)
+
+type Student struct {
+	Id    int
+	Name  string
+	Age   int
+	Score int
+}
+
+var students map[int]Student // 학생 목록을 저장하는 맵
+var lastId int
+
+type Students []Student // ID로 정렬하는 인터페이스
+func (s Students) Len() int {
+	return len(s)
+}
+
+func (s Students) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Students) Less(i, j int) bool {
+	return s[i].Id < s[j].Id
+}
+
+func MakeWebHandler() http.Handler {
+	mux := mux.NewRouter()
+	mux.HandleFunc("/students", GetStudentListHandler).Methods("GET")
+	//-- 여기에 새로운 핸들러 등록 --//
+
+	students = make(map[int]Student) // 임시 데이터 생성
+	students[1] = Student{1, "aaa", 16, 87}
+	students[2] = Student{2, "bbb", 18, 98}
+	lastId = 2
+
+	return mux
+}
+
+func GetStudentListHandler(w http.ResponseWriter, r *http.Request) {
+	list := make(Students, 0) // 학생 목록을 ID로 정렬
+	for _, student := range students {
+		list = append(list, student)
+	}
+	sort.Sort(list)
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(list) // json 포맷으로 변경
+}
+
+func main() {
+	http.ListenAndServe(":3000", MakeWebHandler())
+}
---
+++ ch30/ex30.1/ex30_1_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestJsonHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/students", nil)
+
+	mux := MakeWebHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	var list []Student
+	err := json.NewDecoder(res.Body).Decode(&list) // 결과 반환
+	assert.Nil(err)
+	assert.Equal(2, len(list))
+	assert.Equal("aaa", list[0].Name)
+	assert.Equal("bbb", list[1].Name)
+}
